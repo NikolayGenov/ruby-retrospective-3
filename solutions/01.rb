b626cf9dfbc608022659ab6299d6ba7f8ee18f60
NikolayGenov <nikolay.genov2@gmail.com>
2014-01-21 12:12:09 +0200

    Fix monkey patching to Integer class and indentation problem

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,44 +1,48 @@
 class Integer
-    def prime?
-      self > 1 and (2..Math.sqrt(self)).all? { |div| remainder(div).nonzero? }
-    end
+  def prime?
+    self > 1 and (2..Math.sqrt(self)).all? { |div| remainder(div).nonzero? }
+  end
 
-    def prime_factors
-      factor = (2...abs).find { |div| remainder(div).zero? }
-      factor.nil? ? [abs] : [factor] + (abs / factor).prime_factors
-    end
+  def prime_factors
+    factor = (2...abs).find { |div| remainder(div).zero? }
+    factor.nil? ? [abs] : [factor] + (abs / factor).prime_factors
+  end
 
-    def harmonic
-      (1..self).reduce { |sum,number| sum +  Rational(1,number) }.to_r
-    end
+  def harmonic
+    (1..self).map(&:reciprocal).reduce(&:+)
+  end
 
-    def digits
-      abs.to_s.chars.map { |number| number.to_i }
-    end
+  def reciprocal
+    1 / to_r
+  end
+
+  def digits
+    abs.to_s.chars.map(&:to_i)
+  end
 end
 
 class Array
-    def frequencies
-      reduce(Hash.new(0)) { |value,key| value if value[key] += 1 }
+  def frequencies
+    reduce(Hash.new(0)) { |value,key| value if value[key] += 1 }
+  end
+
+  def average
+    reduce { |sum,number| sum + number } / count.to_f
+  end
+
+  def drop_every(n)
+    select.each_with_index { |_,i| (i + 1).remainder(n).nonzero? }
+  end
+
+  #OPTIMIZE
+  def combine_with(other)
+    list = []
+
+    until count.zero? and other.count.zero? do
+      list << shift unless empty?
+      list << other.shift unless other.empty?
     end
 
-    def average
-      reduce { |sum,number| sum + number } / count.to_f
-    end
-
-    def drop_every(n)
-      select.each_with_index { |_,i| (i + 1).remainder(n).nonzero? }
-    end
-
-    #OPTIMIZE
-    def combine_with(other)
-      list = []
-
-      until count.zero? and other.count.zero? do
-        list << shift unless empty?
-        list << other.shift unless other.empty?
-      end
-
-      list
-    end
+    list
+  end
 end
