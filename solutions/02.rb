f5284a4eda253b385bf605d90ee2f9cf341894b8
NikolayGenov <nikolay.genov2@gmail.com>
2014-01-21 21:41:13 +0200

    Fix solution to the second problem: Rename everything and parser clean the logic

--- solutions/02.rb
+++ solutions/02.rb
@@ -1,72 +1,39 @@
-class Criteria
-  class << self
-    def status(status)
-      Criterion.new { |todo| todo.status.equal? status }
-    end
-
-    def priority(priority)
-      Criterion.new { |todo| todo.priority.equal? priority }
-    end
-
-    def tags(tags)
-      Criterion.new { |todo| tags & todo.tags == tags }
-    end
-  end
-end
-
-class Criterion
-  def initialize(&block)
-    @predicate = block
-  end
-
-  def matches?(todo)
-    @predicate.(todo)
-  end
-
-  def &(other)
-    Criterion.new { |todo| matches?(todo) and other.matches?(todo) }
-  end
-
-  def |(other)
-    Criterion.new { |todo| matches?(todo) or other.matches?(todo) }
-  end
-
-  def !
-    Criterion.new { |todo| not matches?(todo) }
-  end
-end
-
-class Todo
+class Task
   attr_reader :status, :description, :priority, :tags
 
   def initialize(status, description, priority, tags)
-    @status      = status.downcase.to_sym
+    @status      = status
     @description = description
-    @priority    = priority.downcase.to_sym
-    @tags        = tags.split(",").map(&:strip)
+    @priority    = priority
+    @tags        = tags
   end
 end
 
 class TodoList
   include Enumerable
 
-  attr_reader :todos
-
-  def self.parse(todos_string)
-    splited_str = todos_string.lines.map { |line| line.split("|").map(&:strip) }
-    todos       = splited_str.map do |status, description, priority, tags|
-      Todo.new status, description, priority, tags
+  def self.parse(text)
+    parsed_text = Parser.new(text) do |status, description, priority, tags|
+      Task.new status, description, priority, tags
     end
 
-    new todos
+    TodoList.new parsed_text.tasks
   end
 
-  def initialize(todos)
-    @todos = todos
+  def initialize(tasks = [])
+    @tasks = tasks
   end
 
-  def each(&block)
-    @todos.each(&block)
+  def filter(criteria)
+    TodoList.new @tasks.select{ |task| criteria.matches? task }
+  end
+
+  def completed?
+    tasks_completed.equal? tasks.size
+  end
+
+  def adjoin(other)
+    TodoList.new tasks | other.tasks
   end
 
   def tasks_todo
@@ -81,15 +48,73 @@ class TodoList
     (filter Criteria.status :done).to_a.size
   end
 
-  def completed?
-    tasks_completed.equal? todos.size
+  def each(&block)
+    @tasks.each(&block)
   end
 
-  def adjoin(other)
-    TodoList.new todos | other.todos
+  protected
+
+  attr_reader :tasks
+end
+
+class TodoList::Parser
+  attr_reader :tasks
+
+  def initialize(text, &block)
+    @tasks = parse_lines(text).map(&block)
   end
 
-  def filter(criteria)
-    TodoList.new @todos.select{ |todo| criteria.matches? todo }
+  private
+
+  def parse_lines(text)
+    text.lines.map { |line| line.split('|').map(&:strip) }.map do |attributes|
+      process_attributes(*attributes)
+    end
+  end
+
+  def process_attributes(status, description, priority, tags)
+    [status.downcase.to_sym,
+     description,
+     priority.downcase.to_sym,
+     tags.split(',').map(&:strip),
+    ]
+  end
+end
+
+module Criteria
+  class << self
+    def status(status)
+      Criterion.new { |task| task.status.equal? status }
+    end
+
+    def priority(priority)
+      Criterion.new { |task| task.priority.equal? priority }
+    end
+
+    def tags(tags)
+      Criterion.new { |task| (tags & task.tags).eql? tags }
+    end
+  end
+end
+
+class Criterion
+  def initialize(&predicate)
+    @predicate = predicate
+  end
+
+  def matches?(task)
+    @predicate.(task)
+  end
+
+  def &(other)
+    Criterion.new { |task| matches? task and other.matches? task }
+  end
+
+  def |(other)
+    Criterion.new { |task| matches? task or other.matches? task }
+  end
+
+  def !
+    Criterion.new { |task| not matches? task  }
   end
 end
